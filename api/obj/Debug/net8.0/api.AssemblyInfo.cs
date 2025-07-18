98a7e84ae9012f3e56e4e3d1625e646c1681fb69
amine <114872954+0Kaizoku@users.noreply.github.com>
2025-07-18 15:24:42 +0100

    new branch

--- api/obj/Debug/net8.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net8.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bbbbdb3eb8ba75b89e341f87f15684b7e1d8016b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a905630e2c93883c76cba5432faef653bc1869c5")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
