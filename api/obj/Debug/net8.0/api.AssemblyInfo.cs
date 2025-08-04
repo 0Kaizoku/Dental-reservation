6cc209da6580d5f6f183f9d7a6bec31e311e86dc
amine <114872954+0Kaizoku@users.noreply.github.com>
2025-08-04 13:14:38 +0100

    pushing P

--- api/obj/Debug/net8.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net8.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c537419327eecd2f731eeb7b6401ac1fd3298701")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e35f1ac303ebd31d52fad31a0920c7c5d0e4c2da")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
